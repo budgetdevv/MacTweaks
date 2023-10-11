23cb6422cec6e3cda15f5d5f2c2996b8f8b7ef99
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2023-10-12 01:31:03 +0800

    • Added code for requesting accessibility access ( AccessibilityHelpers.cs )
    • Configured csproj to build for ARM64

---
+++ MacTweaks/Helpers/AccessibilityHelpers.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.InteropServices;
+using AppKit;
+using Foundation;
+
+namespace MacTweaks
+{
+    public static class AccessibilityHelpers
+    {
+        private static readonly NSDictionary AccessibilityChecker = new NSDictionary("AXTrustedCheckOptionPrompt", true);
+        
+        public static bool RequestForAccessibilityIfNotGranted()
+        {
+            return AXIsProcessTrustedWithOptions(AccessibilityChecker.Handle);
+        }
+
+        [DllImport("/System/Library/Frameworks/ApplicationServices.framework/ApplicationServices")]
+        private static extern bool AXIsProcessTrustedWithOptions(IntPtr options);
+
+        // public static void RequestForAccessibilityAccess()
+        // {
+        //     NSWorkspace.SharedWorkspace.OpenUrl(new NSUrl("x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility"));
+        // }
+    }
+}
